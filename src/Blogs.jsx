1db8dffecda82e03eef8ceff4f129abd701ecd63
Nishit-Singhal <singhal_nishit2005@outlook.com>
2024-09-02 00:17:43 +0530

    Gaman

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -5,8 +5,12 @@ export default function Blogs(){
     return(
         <div>
             <Navbar />
+            <div className="Blogs">
             <Link to={"/Blogs_Delhi"}><img className="Blogs_Delhi" src="./India_Gate.jpg"></img></Link>
-            <img className="Blogs_Jaipur"></img>
+            <img className="Blogs_Jaipur" src="https://www.holidify.com/images/cmsuploads/compressed/h4_20170822181427.PNG"></img>
+            <img className="Blogs_Kashmir" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhpvfpeKWGevvOQ2mb1B-RaziFse3VYN5DUw&s"></img>
+            <img className="Blogs_MadhyaPradesh" src="https://cdn.budgetyourtrip.com/images/photos/headerphotos/large/india_madhyapradesh.jpg"></img>
+            </div>
         </div>
     )
 }
