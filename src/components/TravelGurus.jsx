9354a3b4870416725d43a0268874504260217e8a
yank2003 <mayanklap23@gmail.com>
2024-09-05 15:51:18 +0530

    color change in div 3 of package

--- src/components/TravelGurus.jsx
+++ src/components/TravelGurus.jsx
@@ -8,76 +8,76 @@ export default function TravelGurus() {
             <h1 align="center" className='Gurus_h1'>Explore With Expert Guides</h1>
             <p align="center" className='Gurus_p'>Unlock the secrets of your destination with our curated selection of local guides. Whether you're exploring the bustling streets of a city, the tranquil beauty of nature, or the hidden gems off the beaten path, our guides offer insider knowledge and personalized experiences tailored to your interests.</p>
             <div align="center" className='Gurus_div1'>
-                <div className='div_part_1'>
+                <div className='Gurus_div_part_1'>
                     <img className='Gurus_divs_img' src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"></img>
                 </div>
-                <div className='div_part_2'>
+                <div className='Gurus_div_part_2'>
                     <h1 align="left" className="Gurus_Heading">Meet Your Guide: Anand</h1>
                     <h2 align="left" className="Gurus_Subhead">Discover the Hidden Treasures of Delhi with Anand</h2>
                     <p align="left" className="Gurus_Description">Embark on an unforgettable journey through Delhi with Anand, a seasoned expert with 5 years of experience in showcasing the best this region has to offer. With a deep passion history, culture, adventure, Anand brings the stories of Delhi to life, offering you an insider's perspective that goes beyond the typical tourist experience.</p>
                 </div>
-                <div className='div_part_3'>
-                    <div className='div_part_3_line1'>
+                <div className='Gurus_div_part_3'>
+                    <div className='Gurus_div_part_3_line1'>
                         <h2>25 comments</h2>
-                        <img className="starrating" src="/4StarRating.png"></img>
+                        <img className="Gurus_starrating" src="/4StarRating.png"></img>
                     </div>
                     <br></br>
                     <br></br>
                     <h2 align="center">Fees: ₹200/hour</h2>
                     <br></br>
                     <br></br>
-                    <div align="center" className='book_now_div'>
-                        <button align="center" className='book_now'>Book Now</button>
+                    <div align="center" className='Gurus_book_now_div'>
+                        <button align="center" className='Gurus_book_now'>Book Now</button>
                     </div>
                 </div>
             </div>
             <br></br>
             <div align="center" className='Gurus_div1'>
-                <div className='div_part_1'>
+                <div className='Gurus_div_part_1'>
                     <img className='Gurus_divs_img' src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"></img>
                 </div>
-                <div className='div_part_2'>
+                <div className='Gurus_div_part_2'>
                     <h1 align="left" className="Gurus_Heading">Meet Your Guide: Aman</h1>
                     <h2 align="left" className="Gurus_Subhead">Discover the Hidden Treasures of Jaipur with Aman</h2>
                     <p align="left" className="Gurus_Description">Embark on an unforgettable journey through Jaipur with Aman, a seasoned expert with 5 years of experience in showcasing the best this region has to offer. With a deep passion history, culture, adventure, Aman brings the stories of Jaipur to life, offering you an insider's perspective that goes beyond the typical tourist experience.</p>
                 </div>
-                <div className='div_part_3'>
-                    <div className='div_part_3_line1'>
+                <div className='Gurus_div_part_3'>
+                    <div className='Gurus_div_part_3_line1'>
                         <h2>20 comments</h2>
-                        <img className="starrating" src="/4StarRating.png"></img>
+                        <img className="Gurus_starrating" src="/4StarRating.png"></img>
                     </div>
                     <br></br>
                     <br></br>
                     <h2 align="center">Fees: ₹250/hour</h2>
                     <br></br>
                     <br></br>
-                    <div align="center" className='book_now_div'>
-                        <button align="center" className='book_now'>Book Now</button>
+                    <div align="center" className='Gurus_book_now_div'>
+                        <button align="center" className='Gurus_book_now'>Book Now</button>
                     </div>
                 </div>
             </div>
             <br></br>
             <div align="center" className='Gurus_div1'>
-                <div className='div_part_1'>
+                <div className='Gurus_div_part_1'>
                     <img className='Gurus_divs_img' src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"></img>
                 </div>
-                <div className='div_part_2'>
+                <div className='Gurus_div_part_2'>
                     <h1 align="left" className="Gurus_Heading">Meet Your Guide: Rahul</h1>
                     <h2 align="left" className="Gurus_Subhead">Discover the Hidden Treasures of Kashmir with Rahul</h2>
                     <p align="left" className="Gurus_Description">Embark on an unforgettable journey through Kashmir with Rahul, a seasoned expert with 5 years of experience in showcasing the best this region has to offer. With a deep passion history, culture, adventure, Rahul brings the stories of Rahul to life, offering you an insider's perspective that goes beyond the typical tourist experience.</p>
                 </div>
-                <div className='div_part_3'>
-                    <div className='div_part_3_line1'>
+                <div className='Gurus_div_part_3'>
+                    <div className='Gurus_div_part_3_line1'>
                         <h2>40 comments</h2>
-                        <img className="starrating" src="/4StarRating.png"></img>
+                        <img className="Gurus_starrating" src="/4StarRating.png"></img>
                     </div>
                     <br></br>
                     <br></br>
                     <h2 align="center">Fees: ₹300/hour</h2>
                     <br></br>
                     <br></br>
-                    <div align="center" className='book_now_div'>
-                        <button align="center" className='book_now'>Book Now</button>
+                    <div align="center" className='Gurus_book_now_div'>
+                        <button align="center" className='Gurus_book_now'>Book Now</button>
                     </div>
                 </div>
             </div>
